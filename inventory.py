a9f2b5709ecfa6898be6a57a2245c88ad481acf1
Mjarrman <73044866+Mjarrman@users.noreply.github.com>
2020-10-20 12:45:22 -0700

    Delete inventory.py

--- inventory.py
+++
@@ -1,8 +0,0 @@
-import items
-
-player_inventory = [items.Rock(), items.WoodenShield(), items.Dagger(), items.Fist(), items.CrustyBread(),
-                    items.CrustyBread(), items.Apple()]
-
-
-def get_inventory():
-    return player_inventory
